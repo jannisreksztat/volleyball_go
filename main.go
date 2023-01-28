a382b79a1f0ea3b452e2589f913d8f3f51abeac4
jannis <rekjannis@gmail.com>
2023-01-28 15:44:57 +0100

    finished dataframe, included remove empty column

--- main.go
+++ main.go
@@ -9,10 +9,24 @@ import (
 
 func main() {
 	generalData, gameData := dvw.ReadDVW("example_data/5004_Hin01_Karlsruhe_Delitzsch.dvw")
-	// generalData, gameData := dvw.ReadDVW("example_data/aim.txt")
 	fmt.Println(generalData[0])
 
 	df := dataframe.LoadRecords(gameData)
-	fmt.Println(df)
+
+	// fmt.Println(df)
+
+	// Get column names
+	// fmt.Println(df.Names())
+
+	// Get column types
+	// fmt.Println(df.Types())
+
+	// Summary
+	// fmt.Println(df.Describe())
+
+	//Returns rows
+	// df.Subset()
+	df = dvw.DeleteEmpty(df, "undefined")
+	dvw.CreateCSV(df, "volleyball.csv")
 
 }
--- read_dvw/read_dvw.go
+++ read_dvw/read_dvw.go
@@ -1,4 +1,4 @@
-package read_dvw
+package dvw
 
 import (
 	"bufio"
@@ -36,15 +36,51 @@ func ReadDVW(dataName string) ([]string, [][]string) {
 		log.Fatal(err)
 	}
 	defer data.Close()
-	return generalData, createMatrix(attackData)
+	return generalData, CreateMatrix(attackData)
 }
 
-func createMatrix(dataString []string) [][]string {
+func CreateMatrix(dataString []string) [][]string {
 	var matrix [][]string
+	header := []string{"Team", "Player", "Skill", "Type", "Rating", "undefined", "undefined",
+		"Start zone", "End zone", "undefined", "End zone +", "undefined", "undefined", "undefined",
+		"undefined", "undefined", "undefined", "Time", "undefined", "undefined", "undefined",
+		"undefined", "undefined", "undefined", "Aufstellung 1", "Aufstellung 2", "Aufstellung 3",
+		"Aufstellung 4", "Aufstellung 5", "Aufstellung 6", "Aufstellung 7", "Aufstellung 8",
+		"Aufstellung 9", "Aufstellung 10", "Aufstellung 11", "Aufstellung 12",
+	}
 
+	matrix = append(matrix, header)
 	for _, content := range dataString {
-		matrix = append(matrix, strings.Split(content, ";"))
+		matrix = append(matrix, SplitContent(content, ";"))
 	}
 
 	return matrix
+}
+
+func SplitContent(input string, delimiter string) []string {
+	splitString := strings.Split(input, ";")
+	output := SpecifyAttack(splitString[0])
+
+	for _, content := range splitString[1:] {
+		output = append(output, content)
+	}
+	return output
+}
+
+func SpecifyAttack(input string) []string {
+	input = strings.ReplaceAll(input, "~", " ")
+	var output []string
+
+	oldPos := 0
+	positionSplit := []int{1, 3, 4, 5, 6, 8, 9, 10, 11, 12}
+	for _, content := range positionSplit {
+		if content < len(input) {
+			output = append(output, input[oldPos:content])
+			oldPos = content
+		} else {
+			output = append(output, " ")
+		}
+	}
+	output = append(output, input[oldPos:])
+	return output
 }
