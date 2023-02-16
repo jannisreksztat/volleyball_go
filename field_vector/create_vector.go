4c6ab01d6be3d12d7690b432ea68b549200e3381
jannis <rekjannis@gmail.com>
2023-02-16 16:53:33 +0100

    create Vectors verbessert

--- field_vector/create_vector.go
+++ field_vector/create_vector.go
@@ -8,20 +8,42 @@ import (
 
 func (volley *Volley) createVectors(df dataframe.DataFrame, attackTeam string) {
 	for row := 0; row < df.Nrow(); row++ {
-		var attack vector
-		if dataFunction.GetData(df, "Start zone", row) == "" {
-			attack.Startzone = "8"
-		} else {
-			attack.Startzone = dataFunction.GetData(df, "Start zone", row)
-		}
-		if dataFunction.GetData(df, "End zone", row) == "" {
-			attack.Endzone = attackTeam + "8"
-		} else {
-			attack.Endzone = dataFunction.GetData(df, "Team", row) + dataFunction.GetData(df, "End zone", row) +
-				dataFunction.GetData(df, "End zone +", row)
-		}
-		attack.Type = dataFunction.GetData(df, "Type", row)
-		attack.Rating = dataFunction.GetData(df, "Rating", row)
-		volley.Aktion = append(volley.Aktion, attack)
+		volley.Aktion = append(volley.Aktion, vector{
+			volley.writeStartzone(row, df),
+			volley.writeEndzone(row, df, attackTeam),
+			volley.writeType(row, df),
+			volley.writeRating(row, df),
+		})
 	}
+}
+
+func (volley *Volley) writeStartzone(row int, df dataframe.DataFrame) string {
+	if dataFunction.GetData(df, "Start zone", row) == "" {
+		return "8"
+	} else {
+		return dataFunction.GetData(df, "Start zone", row)
+	}
+}
+
+func (volley *Volley) writeEndzone(row int, df dataframe.DataFrame, attackTeam string) string {
+	if dataFunction.GetData(df, "End zone", row) == "" {
+		return attackTeam + "8"
+	} else {
+		return dataFunction.GetData(df, "Team", row) + dataFunction.GetData(df, "End zone", row) +
+			dataFunction.GetData(df, "End zone +", row)
+	}
+}
+
+func (volley *Volley) writeType(row int, df dataframe.DataFrame) string {
+	return dataFunction.GetData(df, "Type", row)
+}
+
+func (volley *Volley) writeRating(row int, df dataframe.DataFrame) string {
+	rating := dataFunction.GetData(df, "Rating", row)
+	if rating == "#" {
+		return `\#`
+	} else {
+		return rating
+	}
+
 }
